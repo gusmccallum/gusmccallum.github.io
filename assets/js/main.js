bd7191d6f025680514a471404f018f0647155c56
gusmccallum <gusmccallum@hotmail.com>
2022-06-20 17:17:57 -0400

    Removed responsive nav for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,34 +31,6 @@
 			speed: 300
 		});
 
-	// Nav.
 
-		// Toggle.
-			$(
-				'<div id="navToggle">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
 
 })(jQuery);
